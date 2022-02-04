3544c0b583f68a642768cedb3b49f9112228213c
LeventelisV <61799850+LeventelisV@users.noreply.github.com>
2022-02-04 15:34:09 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 import firebase from 'firebase/compat/app'
-import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 import './App.css';
 import SignIn from './Components/SignIn'
 import SignOut from './Components/SignOut'
 import ChatMain from './Components/ChatMain'
-
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 
 
 const auth = firebase.auth();
-const firestore = firebase.firestore();
-
-
 
 function App() {
+  
   const [user] = useAuthState(auth);
 
-
-  
- 
   return (
     <div className="App">
       {/* <header className="App-header">
 
       </header> */}
-      <section>{user ? <ChatMain user={user} /> : <SignIn />}</section>
+      <section>{user ? <ChatMain user={user} auth={auth}/> : <SignIn />}</section>
     </div>
   );
 }
--- src/Components/ChatMain.jsx
+++ src/Components/ChatMain.jsx
@@ -2,17 +2,33 @@ import { useState } from "react"
 import ChatHeader from "./ChatHeader"
 import ChatInput from "./ChatInput"
 import ChatMessages from "./ChatMessages"
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore';
 
-export default function ChatMain({ user }) {
+firebase.initializeApp({
+    apiKey: "AIzaSyA9Bigk5Erf9GmxrqNGaRg00aWeE8nXglE",
+    authDomain: "shutthechatup.firebaseapp.com",
+    projectId: "shutthechatup",
+    storageBucket: "shutthechatup.appspot.com",
+    messagingSenderId: "150965025960",
+    appId: "1:150965025960:web:5bb3c24ad37742b92f169d",
+    measurementId: "G-4YSH27MT9M"
+})
 
+const firestore = firebase.firestore();
+
+export default function ChatMain({ user ,auth}) {
+    
+    const messagesRef = firestore.collection('messages')
+    
     return (
         <>
             <div className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen">
                 <ChatHeader user={user} />
 
-                <ChatMessages user={user}/>
+                <ChatMessages user={user} messagesRef={messagesRef}/>
 
-                <ChatInput />
+                {/* <ChatInput auth={auth} messagesRef={messagesRef}/> */}
 
             </div>
 
