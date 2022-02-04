d4bb3f311291089cc65a7f4c8957b80f88f386ee
LeventelisV <61799850+LeventelisV@users.noreply.github.com>
2022-02-04 11:14:44 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 import './App.css';
+import SignIn from './Components/SignIn';
+import ChatMain from './Components/ChatMain'
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA9Bigk5Erf9GmxrqNGaRg00aWeE8nXglE",
+  authDomain: "shutthechatup.firebaseapp.com",
+  projectId: "shutthechatup",
+  storageBucket: "shutthechatup.appspot.com",
+  messagingSenderId: "150965025960",
+  appId: "1:150965025960:web:5bb3c24ad37742b92f169d",
+  measurementId: "G-4YSH27MT9M"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
+console.log(user)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+
+      </header> */}
+      <section>{user ? <ChatMain user={user}/> : <SignIn />}</section>
     </div>
   );
 }
