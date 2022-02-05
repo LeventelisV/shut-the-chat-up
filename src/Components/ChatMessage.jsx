bd46ced9349029de6b36b7f803f099a39f142e94
LeventelisV <61799850+LeventelisV@users.noreply.github.com>
2022-02-05 14:24:46 +0100

    wip

--- src/Components/ChatMessage.jsx
+++ src/Components/ChatMessage.jsx
@@ -4,12 +4,12 @@ export default function ChatMessage({ message, user }) {
     console.log('---ChatMessage')
     const auth = getAuth();
 
-    const messageClass = 'uid' === auth.currentUser.uid ? 'sent' : 'received';
-
+    const messageStatus = message.uid === auth.currentUser.uid ? 'sent' : 'received';
+    
     let date = new Date(message?.createdAt?.seconds * 1000)
 
     return (
-        <div className="chat-message">
+        <div className={"chat-message"}>
             <div className="flex items-end">
                 <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
                     <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">{message.text}</span></div>
