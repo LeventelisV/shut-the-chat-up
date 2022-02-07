66194229b3706d69ea62af2d9fa73a71a1afe37a
LeventelisV <61799850+LeventelisV@users.noreply.github.com>
2022-02-07 16:05:44 +0100

    input form

--- src/Components/ChatInput.jsx
+++ src/Components/ChatInput.jsx
@@ -5,11 +5,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 
-export default function ChatInput({ auth, messagesRef}) {
+export default function ChatInput({ auth, messagesRef }) {
     console.log('--ChatInput')
     const [inputValue, setInputValue] = useState('')
 
-    const sendMessage = async () => {
+    const sendMessage = async (e) => {
+        e.preventDefault()
+
         if (inputValue.replace(/\s/g, "").length) {
             const { uid, photoURL } = auth.currentUser;
             let mid = uuidv4()
@@ -26,7 +28,7 @@ export default function ChatInput({ auth, messagesRef}) {
 
     return (<>
         <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
-            <div className="relative flex">
+            <form className="relative flex" onSubmit={sendMessage}>
                 {/* <span className="absolute inset-y-0 flex items-center">
                     <button type="button" className="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-600">
@@ -54,12 +56,12 @@ export default function ChatInput({ auth, messagesRef}) {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                     </button> */}
-                    <button type="button" onClick={sendMessage} className="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
+                    <button type="submit" className="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 transform rotate-90">
                             <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
                         </svg>
                     </button>
                 </div>
-            </div>
+            </form>
         </div></>)
 }
